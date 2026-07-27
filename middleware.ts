a2c6c5bb43d226ad384e4b47a9dbdcfd0084093e
Kreonex Technologies <kreonextech@gmail.com>
2026-07-27 23:21:26 +0530

    Fix middleware auth redirect loop by adding fallback NEXTAUTH_SECRET to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { withAuth } from 'next-auth/middleware';
 
 export default withAuth({
+  secret:
+    process.env.NEXTAUTH_SECRET ||
+    'c4a37ee3fbac7b5a2fd29053fe4364f6fb31fff7615fa32b665ff2425480b89dfea41fed5036b21b',
   pages: {
     signIn: '/login',
   },
